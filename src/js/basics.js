b2445eb000652f4138bddeff294bb0e740ffc25d
Sebastian Brümmer <sebastian@produktivbuero.de>
2018-12-06 09:31:11 +0100

    Import v0.9.0

---
+++ src/js/basics.js
@@ -0,0 +1,56 @@
+const $cookiesLink = document.getElementById('livezilla.cookies.link');
+const $cookiesStatus = document.getElementById('livezilla.cookies.status');
+const $trackingLink = document.getElementById('livezilla.tracking.link');
+const $trackingStatus = document.getElementById('livezilla.tracking.status');
+
+class pbLiveZilla {
+  static status() {
+    if (LiveZilla.OptOutCookie) {
+      $cookiesLink.innerHTML = window.pb.livezilla.cookies.enable;
+      $cookiesStatus.innerHTML = window.pb.livezilla.status.disabled;
+    } else {
+      $cookiesLink.innerHTML = window.pb.livezilla.cookies.disable;
+      $cookiesStatus.innerHTML = window.pb.livezilla.status.enabled;
+    }
+    console.log("Opt Out Cookie: " + LiveZilla.OptOutCookie);
+
+    if (LiveZilla.OptOutTrack) {
+      $trackingLink.innerHTML = window.pb.livezilla.tracking.enable;
+      $trackingStatus.innerHTML = window.pb.livezilla.status.disabled;
+    } else {
+      $trackingLink.innerHTML = window.pb.livezilla.tracking.disable;
+      $trackingStatus.innerHTML = window.pb.livezilla.status.enabled;
+    }
+    console.log("Opt Out Track: " + LiveZilla.OptOutTrack);
+  }
+
+  static toggleCookies() {
+    if (LiveZilla.OptOutCookie) {
+      LiveZilla.OptOutCookie = false;
+      pbLiveZilla.status();
+    } else {
+      LiveZilla.OptOutCookies();
+      pbLiveZilla.status();
+    }
+  }
+
+  static toogleTracking() {
+    if (LiveZilla.OptOutTrack) {
+      LiveZilla.OptOutTrack = false;
+      pbLiveZilla.status();
+    } else {
+      LiveZilla.OptOutTracking();
+      pbLiveZilla.status();
+    }
+  }
+}
+
+/* init (check every 500ms) */
+var checkExist = setInterval(function() {
+   if (typeof LiveZilla != "undefined") {
+      console.log("LiveZilla loaded.");
+      clearInterval(checkExist);
+
+      pbLiveZilla.status();
+   }
+}, 500);
---
+++ src/pblivezilla.php
@@ -0,0 +1,177 @@
+<?php
+/**
+ * @package    PB LiveZilla
+ *
+ * @author     Sebastian Brümmer <sebastian@produktivbuero.de>
+ * @copyright  Copyright (C) 2018 *produktivbüro . All rights reserved
+ * @license    GNU General Public License version 2 or later
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Application\CMSApplication;
+use Joomla\CMS\Plugin\CMSPlugin;
+
+/**
+ * PB LiveZilla plugin.
+ *
+ * @package  PB LiveZilla
+ * @since    0.9.0
+ */
+class plgSystemPbLiveZilla extends CMSPlugin
+{
+  /**
+   * Application object
+   *
+   * @var    CMSApplication
+   * @since  0.9.0
+   */
+  protected $app;
+
+  /**
+   * Affects constructor behavior. If true, language files will be loaded automatically.
+   *
+   * @var    boolean
+   * @since  0.9.0
+   */
+  protected $autoloadLanguage = true;
+
+  /**
+   * This function is called on initialization.
+   *
+   * @return  void.
+   *
+   * @since   0.9.0
+   */
+
+  public function __construct(&$subject, $config = array())
+  {
+
+    parent::__construct($subject, $config);
+
+    $params = new JRegistry($config['params']);
+
+    $this->livezilla = array();
+
+    // Basic parameters
+    $this->livezilla['code'] = $params->get('code');
+    $this->livezilla['exclude'] = $params->get('exclude');
+    $this->livezilla['optout_cookie'] = $params->get('optout_cookie', '1');
+    $this->livezilla['optout_tracking'] = $params->get('optout_tracking', '1');
+      
+  }
+
+
+  /**
+   * This event is triggered before the framework creates the head section of the Document.
+   *
+   * @return  void.
+   *
+   * @since   0.9.0
+   */
+  public function onBeforeCompileHead()
+  {
+    // fast fail
+    if ($this->app->isAdmin()) {
+      return;
+    }
+
+    // Excluded menu items
+    $current = $this->app->getMenu()->getActive()->id;
+    if (in_array($current, $this->livezilla['exclude'])) {
+      return;
+    }
+
+    $doc = JFactory::getDocument();
+
+    $settings = array();
+
+    // Plugin parameters
+    $settings = $this->livezilla;
+
+    // Language strings
+    $settings['cookies']['disable'] = JText::_('PLG_SYSTEM_PBLIVEZILLA_OPTOUT_COOKIES_LINK_DISABLE');
+    $settings['cookies']['enable'] = JText::_('PLG_SYSTEM_PBLIVEZILLA_OPTOUT_COOKIES_LINK_ENABLE');
+    $settings['tracking']['disable'] = JText::_('PLG_SYSTEM_PBLIVEZILLA_OPTOUT_TRACKING_LINK_DISABLE');
+    $settings['tracking']['enable'] = JText::_('PLG_SYSTEM_PBLIVEZILLA_OPTOUT_TRACKING_LINK_ENABLE');
+    $settings['status']['disabled'] = JText::_('PLG_SYSTEM_PBLIVEZILLA_OPTOUT_DISABLED');
+    $settings['status']['enabled'] = JText::_('PLG_SYSTEM_PBLIVEZILLA_OPTOUT_ENABLED');
+
+    // Insert global settings object
+    $script = 'window.pb = window.pb || {}; window.pb.livezilla = '. json_encode($settings, JSON_FORCE_OBJECT);
+    $doc->addScriptDeclaration( $script );
+
+    // Insert script
+    $doc->addScript(Juri::base(true).'/media/plg_system_pblivezilla/js/basics.js', 'text/javascript', true, true);
+  }
+
+
+  /**
+   * This event is triggered after the framework has rendered the application.
+   * When this event is triggered the output of the application is available in the response buffer.
+   *
+   * @return  void.
+   *
+   * @since   0.9.0
+   */
+  public function onAfterRender()
+  {
+    // fast fail
+    if ($this->app->isAdmin() || empty($this->livezilla['code'])) {
+      return;
+    }
+
+    // Excluded menu items
+    $current = $this->app->getMenu()->getActive()->id;
+    if (in_array($current, $this->livezilla['exclude'])) {
+      return;
+    }
+
+    // Insert code
+    $buffer = $this->app->getBody();
+    $buffer = str_ireplace('</body>', $this->livezilla['code'] . '</body>', $buffer);
+    $this->app->setBody($buffer);
+  }
+
+
+  /**
+   * This is the first stage in preparing content for output and is the
+   * most common point for content orientated plugins to do their work.
+   *
+   * @param   string   $context  The context of the content being passed to the plugin.
+   * @param   object   &$row     The article object.  Note $article->text is also available
+   * @param   mixed    &$params  The article params
+   * @param   integer  $page     The 'page' number
+   *
+   * @return  void.
+   *
+   * @since   0.9.0
+   */
+  public function onContentPrepare($context, &$row, &$params, $page = 0)
+  {
+    // fast fail
+    if ($this->app->isAdmin() || JString::strpos($row->text, '{plg_system_pblivezilla') === false) {
+      return;
+    }
+
+    // Load language from the backend
+    $lang = JFactory::getLanguage();
+    $lang->load('plg_'.$this->_type.'_'.$this->_name, JPATH_SITE);
+
+    // Replace shortcodes
+    if ( $this->livezilla['optout_cookie'] && JString::strpos($row->text, '{plg_system_pblivezilla_optout_cookies') !== false ) {
+      $insert = '<a href="javascript:pbLiveZilla.toggleCookies()" id="livezilla.cookies.link">'.JText::_('PLG_SYSTEM_PBLIVEZILLA_OPTOUT_COOKIES_LINK_DISABLE').'</a><span id="livezilla.cookies.status">'.JText::_('PLG_SYSTEM_PBLIVEZILLA_OPTOUT_ENABLED').'</span>';
+      $regex = '/{plg_system_pblivezilla_optout_cookies}/im';
+      $row->text = preg_replace($regex, $insert, $row->text);
+    }
+
+    if ( $this->livezilla['optout_tracking'] && JString::strpos($row->text, '{plg_system_pblivezilla_optout_tracking') !== false ) {
+      $insert = '<a href="javascript:pbLiveZilla.toogleTracking()" id="livezilla.tracking.link">'.JText::_('PLG_SYSTEM_PBLIVEZILLA_OPTOUT_TRACKING_LINK_DISABLE').'</a><span id="livezilla.tracking.status">'.JText::_('PLG_SYSTEM_PBLIVEZILLA_OPTOUT_ENABLED').'</span>';
+      $regex = '/{plg_system_pblivezilla_optout_tracking}/im';
+      $row->text = preg_replace($regex, $insert, $row->text);
+    }
+
+  
+  }
+
+}
